b8e2dec22843c239d4540544b0e7184acc19af39
Nataya Flores <natayafs@gmail.com>
2025-04-10 21:40:07 -0300

    Agrega las barras de vida y energía

--- hud.gd
+++ hud.gd
@@ -1,6 +1,5 @@
 extends CanvasLayer
 
-# Notifies `Main` node that the button has been pressed
 signal start_game
 
 func show_message(text):
@@ -9,12 +8,9 @@ func show_message(text):
 	$MessageTimer.start()
 
 func show_game_over():
-	show_message("Game Over")
-	# Wait until the MessageTimer has counted down.
+	show_message("Perdiste :(")
 	await $MessageTimer.timeout
-
 	$Message.show()
-	# Make a one-shot timer and wait for it to finish.
 	await get_tree().create_timer(1.0).timeout
 	$StartButton.show()
 
@@ -23,6 +19,7 @@ func update_score(score):
 
 func _on_start_button_pressed():
 	$StartButton.hide()
+	$Message.hide()
 	start_game.emit()
 
 func _on_message_timer_timeout():
--- player.gd
+++ player.gd
@@ -1,16 +1,25 @@
 extends Area2D
-signal hit
 
-@export var speed = 400 # How fast the player will move (pixels/sec).
-var screen_size # Size of the game window.
+signal hit
+signal health_changed(health)
+signal stamina_changed(stamina)
+
+var max_health = 1000
+var health = 1000
+var max_stamina = 1000
+var stamina = 250
+var touching_red = false
+var touching_blue = false
+
+@export var speed = 400
+var screen_size
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	hide()
 
-
 func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
+	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -36,15 +45,43 @@ func _process(delta):
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = "up"
 		$AnimatedSprite2D.flip_v = false
+		
+	#Daño por tocar zona roja
+	if touching_red:
+		health -= 100 * delta
+		health = max(health, 0)
+		emit_signal("health_changed", health)
+		if health == 0:
+			_on_body_entered(self)
 
+	# Recuperación de energía
+	if touching_blue:
+		stamina += 150 * delta
+		stamina = min(stamina, max_stamina)
+		emit_signal("stamina_changed", stamina)
 
 func _on_body_entered(_body: Node2D) -> void:
-	hide() # Player disappears after being hit.
+	hide()
 	hit.emit()
-	# Must be deferred as we can't change physics properties on a physics callback.
 	$CollisionShape2D.set_deferred("disabled", true)
 
 func start(pos):
 	position = pos
 	show()
 	$CollisionShape2D.disabled = false
+	
+func _on_red_area_body_entered(body):
+	if body == self:
+		touching_red = true
+
+func _on_red_area_body_exited(body):
+	if body == self:
+		touching_red = false
+
+func _on_blue_area_body_entered(body):
+	if body == self:
+		touching_blue = true
+
+func _on_blue_area_body_exited(body):
+	if body == self:
+		touching_blue = false
